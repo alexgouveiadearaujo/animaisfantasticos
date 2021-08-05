9dbd7811d79a3f47c58d03e749c0cba52c09781b
Alex <alexgouveiadearaujo@gmail.com>
2021-08-05 18:33:47 -0300

    filename modification

---
+++ js/modules/fetch-animals.js
@@ -0,0 +1,38 @@
+import initNumbers from './numbers.js';
+
+
+export default function initFetchAnimals(){
+    
+    async function fetchAnimais(url){
+        try{
+            const animaisResponse =  await fetch(url);
+            const animaisJson = await animaisResponse.json();
+            const numGrid = document.querySelector('.numbers-grid');
+
+            animaisJson.forEach(animal => {
+            const divAnimal =  createAnimal(animal);
+            numGrid.appendChild(divAnimal);
+            });
+
+            initNumbers();
+        }catch(err){
+            console.log(Error(err));
+        }
+
+    }
+
+    function createAnimal(animal){
+
+        const div = document.createElement('div');
+        div.classList.add('number-animal');
+
+        div.innerHTML = `<h3>${animal.specie}</h3><span data-number >${animal.total}</span>`;
+
+        return div;
+    }
+    
+    fetchAnimais('./js/animaisApi.json');
+
+
+
+}
